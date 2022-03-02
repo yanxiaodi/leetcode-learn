e3b6fb3b0a23cf25f0621678b5457edb5e601c9f
xiaodi.yan <yan_xiaodi@hotmail.com>
2022-03-02 23:30:08 +1300

    Create folders

---
+++ csharp/array/1.Two Sum/1.two-sum.cs
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1 lang=csharp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+public class Solution
+{
+  public int[] TwoSum(int[] nums, int target)
+  {
+    var dic = new Dictionary<int, int>();
+    var result = new int[2];
+    for (int i = 0; i < nums.Length; i++)
+    {
+      if (dic.ContainsKey(nums[i]))
+      {
+        result[0] = i;
+        result[1] = dic[nums[i]];
+      }
+      dic[target - nums[i]] = i;
+    }
+    return result;
+  }
+}
+// @lc code=end
+
