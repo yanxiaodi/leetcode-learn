4cbebb6eab3f06694bb4b3c43fdd782087ebc3e7
xiaodi.yan <yan_xiaodi@hotmail.com>
2022-03-03 18:32:45 +1300

    [191] Number of 1 Bits

--- csharp/bit-manipulation/191.Number of 1 Bits/191.number-of-1-bits.cs
+++ csharp/bit-manipulation/191.Number of 1 Bits/191.number-of-1-bits.cs
@@ -12,11 +12,35 @@ public class Solution
     var count = 0;
     while (n != 0)
     {
-      n = n & (n - 1);
-      count++;
+      if ((n & 1) != 0)
+      {
+        count++;
+      }
+      n >>= 1;
     }
     return count;
+
+    // var count = 0;
+    // while (n != 0)
+    // {
+    //   n = n & (n - 1);
+    //   count++;
+    // }
+    // return count;
+
     //return n.ToString().Replace("0", "").Length;
+
+    // var count = 0;
+    // uint test = 1;
+    // for (int i = 0; i < 32; i++)
+    // {
+    //   if ((test & n) != 0)
+    //   {
+    //     count++;
+    //   }
+    //   test <<= 1;
+    // }
+    // return count;
   }
 }
 // @lc code=end
