919959f4126c2219949688afde5c9523e236bde8
xiaodi.yan <yan_xiaodi@hotmail.com>
2022-03-09 22:39:29 +1300

    [1588] Sum of All Odd Length Subarrays

--- csharp/Unknown/1588.Sum of All Odd Length Subarrays/1588.sum-of-all-odd-length-subarrays.cs
+++ csharp/Unknown/1588.Sum of All Odd Length Subarrays/1588.sum-of-all-odd-length-subarrays.cs
@@ -13,20 +13,40 @@ public class Solution
     {
       return 0;
     }
-    var result = 0;
-    for (int i = 0; i < arr.Length; i++)
+    var sum = 0;
+    var sub = 1;
+    var n = arr.Length;
+    while (sub <= n)
     {
-      var temp = 0;
-      for (int j = i; j < arr.Length; j++)
+      int temp = 0;
+      for (int i = 0; i < sub; i++)
       {
-        temp += arr[j];
-        if ((j - i + 1) % 2 > 0)
-        {
-          result += temp;
-        }
+        temp += arr[i];
       }
+      sum += temp;
+      for (int i = 0; i < n - sub; i++)
+      {
+        temp += arr[i + sub] - arr[i];
+        sum += temp;
+      }
+      sub += 2;
     }
-    return result;
+    return sum;
+
+    // var result = 0;
+    // for (int i = 0; i < arr.Length; i++)
+    // {
+    //   var temp = 0;
+    //   for (int j = i; j < arr.Length; j++)
+    //   {
+    //     temp += arr[j];
+    //     if ((j - i + 1) % 2 > 0)
+    //     {
+    //       result += temp;
+    //     }
+    //   }
+    // }
+    // return result;
   }
 }
 // @lc code=end
